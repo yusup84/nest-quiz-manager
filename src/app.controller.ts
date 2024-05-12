316405f43bd84d81ab4d826c60d03a282f0e7959
yusup84 <globalinformatika84@gmail.com>
2024-05-12 07:49:22 +0700

    init project

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('say')
+  getSay(): string {
+    return this.appService.getSay();
+  }
+}
