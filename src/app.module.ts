4684e56203e6d3e9f73b5c556cefd0b59881dd3c
yusup84 <globalinformatika84@gmail.com>
2024-05-12 08:24:08 +0700

    02 Creating a Module and Controller in Nest JS | Node JS, Javascript, Typescript

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { QuizModule } from './modules/quiz/quiz.module';
+import { QuizController } from './modules/quiz/quiz.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [QuizModule],
+  controllers: [AppController, QuizController],
   providers: [AppService],
 })
 export class AppModule {}
