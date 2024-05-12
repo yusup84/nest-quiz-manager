e892d0d3dcac7aae9b086fdc81cab2a4be510db8
yusup84 <globalinformatika84@gmail.com>
2024-05-12 08:51:26 +0700

    03 Depedency Injection in Nest JS | Inject Service in Controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { QuizModule } from './modules/quiz/quiz.module';
-import { QuizController } from './modules/quiz/quiz.controller';
 
 @Module({
   imports: [QuizModule],
-  controllers: [AppController, QuizController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/modules/quiz/quiz.controller.ts
+++ src/modules/quiz/quiz.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { QuizService } from './quiz.service';
 
 @Controller('quiz')
 export class QuizController {
+  constructor(private readonly quizService: QuizService) {}
   @Get()
   getAll() {
-    return [1, 2, 3];
+    return this.quizService.getAllQuiz();
   }
 }
